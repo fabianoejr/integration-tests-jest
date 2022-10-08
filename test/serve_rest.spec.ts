eb184420467458fa50b8790d56c361fd52df47ff
Fabiano Elias Junior <fabianoeliasjunior@hotmail.com>
2022-10-08 11:41:06 -0300

    testes

--- test/serve_rest.spec.ts
+++ test/serve_rest.spec.ts
@@ -1,8 +1,13 @@
 import pactum from 'pactum';
+import { inspect } from 'util';
 import { SimpleReporter } from '../simple-reporter';
+import { faker } from '@faker-js/faker';
 
 describe('ServeRest API', () => {
   let token = '';
+  let produto = '';
+  let i = 0;
+  let arrProduto = [];
   const p = pactum;
   const rep = SimpleReporter;
   const baseUrl = 'https://serverest.dev';
@@ -16,8 +21,8 @@ describe('ServeRest API', () => {
         .spec()
         .post(`${baseUrl}/login`)
         .withJson({
-          email: 'a@b.com.br',
-          password: 'admin.1234'
+          email: 'fulano@qa.com',
+          password: 'teste'
         })
         .expectStatus(200)
         .returns('authorization');
@@ -31,6 +36,46 @@ describe('ServeRest API', () => {
         .get(`${baseUrl}/usuarios`)
         .withHeaders('Authorization', token)
         .expectStatus(200);
+    });
+  });
+
+  describe('produtos', () => {
+    for (i; i <= 2; i++) {
+      it('post', async () => {
+        produto = await p
+          .spec()
+          .post(`${baseUrl}/produtos`)
+          .withHeaders('Authorization', token)
+          .withJson({
+            nome: faker.commerce.product() + 150801,
+            preco: 5000,
+            descricao: faker.commerce.product() + 150801,
+            quantidade: 1
+          })
+          .expectStatus(201)
+          .expectBodyContains('Cadastro realizado com sucesso')
+          .inspect()
+          .returns('_id');
+        arrProduto.push(produto);
+        console.log(arrProduto);
+      });
+    }
+  });
+
+  describe('carrinhos', () => {
+    arrProduto.forEach(function (idproduto) {
+      it('post', async () => {
+        await p
+          .spec()
+          .post(`${baseUrl}/carrinhos`)
+          .withHeaders('Authorization', token)
+          .withJson({
+            idProduto: idproduto,
+            quantidade: 1
+          })
+          .expectStatus(201)
+          .inspect();
+      });
     });
   });
 });
